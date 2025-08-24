a1accf4cd028d2ee8958a88bcf42fbbf25464b00
amir mashmul <amirmashmul6@gmail.com>
2025-08-24 17:03:37 +0430

    update tailwaind config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/bestino-catalog/'
+  plugins: [react(), tailwind()],
+  base: '/bestino-catalog/',
 })
